7c7ce79853c086fb17ecc76e7e6b3931bb693d2c
Marcin Towcik <marcin.towcik@capgemini.com>
2017-10-24 13:27:43 +0200

    next

--- config.js
+++ config.js
@@ -9,7 +9,10 @@ exports.config = {
     // Spec patterns are relative to the configuration file location passed
     // to protractor (in this example conf.js).
     // They may include glob patterns.
-    specs: 'spec.js',
+    // specs: 'spec.js',
+    suites:{
+        Jasmine: 'spec/jasmine/dataDrivenSpec.js'
+    },
     // Options to be passed to Jasmine-node.
     jasmineNodeOpts: {
     showColors: true, // Use colors in the command line report.
