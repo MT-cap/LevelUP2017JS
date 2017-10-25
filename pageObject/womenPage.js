b82740c4df1442e68ff4b6310d9c57d194d19524
Marcin Towcik <marcin.towcik@capgemini.com>
2017-10-25 09:49:01 +0200

    multi problemi with ex1Test

--- pageObject/womenPage.js
+++ pageObject/womenPage.js
@@ -1,5 +1,6 @@
 var WomenPage = function(){
     this.URL = 'http://automationpractice.com/index.php?id_category=3&controller=category';
-    this.womenTopsButton = element(by.linkText('Tops'));
+    // this.womenTopsButton = element(by.linkText('Tops'));
+    this.womenTopsButton = element(By.css('.submenu-container.clearfix.first-in-line-xs >li:nth-of-type(1) >a.sf-with-ul'));
 }
 module.exports = WomenPage;
--- spec/pageObjectTest/ex1Test.js
+++ spec/pageObjectTest/ex1Test.js
@@ -2,50 +2,54 @@ var HomePage = require(pageObjectDir+"/homePage.js"),
     WomenPage = require(pageObjectDir+"/womenPage.js"),
     WomenTopsPage = require(pageObjectDir+"/womenTopsPage.js"),
     WomenTopsProductPage = require(pageObjectDir+"/womenTopsProductPage.js"),
-    
     homePage = new HomePage(),
     womenPage = new WomenPage(),
     womenTopsPage = new WomenTopsPage(),
     womenTopsProductPage = new WomenTopsProductPage();
 
+beforeAll(function () {
+    browser.get(homePage.URL);
+})
+
 describe('Protractor: http://automationpractice.com', function() {
-    it('exercise 1', function() {
-
-        browser.waitForAngularEnabled(false);
-
-        // Wejsc na strone http://automationpractice.com
-        browser.get(homePage.URL);
-
         // Otworzyć dział damski – zwalidować czy na pewno jesteśmy na odpowiedniej stronie
-        homePage.womenTab.click();
-        expect(browser.getTitle())
-            .toEqual('Women - My Store');
+        
+        // expect(browser.getTitle())
+        //     .toEqual('Women - My Store');
+        it('Check if \'Women - My Store\' page is displayed?', function () {
+            homePage.womenTab.click();
+            expect(browser.getTitle())
+                .toEqual('Women - My Store');
+        })
 
         // Otworzyć dział topy – zwalidowac
-        womenPage.womenTopsButton.click();
-        expect(browser.getTitle())
-            .toEqual('Tops - My Store');
+        it('Check if \'Tops - My Store\' page is displayed?', function () {
+            womenPage.womenTopsButton.click().then(expect(browser.getTitle())
+                .toEqual('Tops - My Store'));
+            
+        })
 
         // Otworzyć Faded Short Sleeve T-shirts
-        womenTopsPage.fadedLongSleeveTshirt.click();
-        expect(browser.getTitle())
-            .toEqual('Faded Short Sleeve T-shirts - My Store');
-
+        it('Check if \'Faded Short Sleeve T-shirts - My Store\' page is displayed?', function () {
+            womenTopsPage.fadedLongSleeveTshirt.click();
+            expect(browser.getTitle())
+                .toEqual('Faded Short Sleeve T-shirts - My Store');
+        })
         // Na stronie produktu spawrdzić czy opis zawiera wiecej niż 8 znaków i czy stan jest „new”
-        expect(womenTopsProductPage.productDescription
-            .getText()
-            .length)
-            .toBeGreaterThan(8);
-        expect(womenTopsProductPage.productCondition
-            .getText())
-            .toEqual('New');
+        // expect(womenTopsProductPage.productDescription
+        //     .getText()
+        //     .length)
+        //     .toBeGreaterThan(8);
+        // expect(womenTopsProductPage.productCondition
+        //     .getText())
+        //     .toEqual('New');
 
-        // Dodać produkt w rozmiarze m do koszyka
-        womenTopsProductPage.sizeDropdownM.click();
-        womenTopsProductPage.addToCartButton.click();
+        // // Dodać produkt w rozmiarze m do koszyka
+        // womenTopsProductPage.sizeDropdownM.click();
+        // womenTopsProductPage.addToCartButton.click();
 
         // Przejsc do koszyka
-        womenTopsProductPage.proceedToCheckoutButton.click();
+        // womenTopsProductPage.proceedToCheckoutButton.click();
         
         // Sprawdzić czy znajduje się tam produkt
         // https://jasmine.github.io/2.0/introduction.html
@@ -61,5 +65,4 @@ describe('Protractor: http://automationpractice.com', function() {
         //     .toBe(true);
         // expect(contactUs.sendButton.isPresent())
         //     .toBe(true);  
-    });
 });
